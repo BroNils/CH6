70209bc4732362423854bd513a223393ddf18c47
mrakhaf <mrakhaf.0@gmail.com>
2022-05-13 20:53:20 +0700

    fix: invalid path when doing unit testing API

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,11 +1,12 @@
 const request = require("supertest");
 const app = require("../app");
 const JEST_TIMEOUT = 30000;
+const ROOT_API = process.env.ROOT_PATH;
 
 // load test file
-const login = require("./api/login.test");
-const logout = require("./api/logout.test");
-const nf = require("./api/nf.test");
+const login = require(process.env.ROOT_PATH + "./login.test");
+const logout = require(process.env.ROOT_PATH + "./logout.test");
+const nf = require(process.env.ROOT_PATH + "./nf.test");
 
 describe("API Scope Testing", () => {
   login(app, request, JEST_TIMEOUT);
